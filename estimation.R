5834d25e5f547806b3e20ad0215b4ebdc7939ead
Louis Geist <louisjjgeist@gmail.com>
2023-06-16 12:23:14 +0200

    code reformat

--- estimation.R
+++ estimation.R
@@ -6,50 +6,108 @@ library(fUnitRoots)
 library(mfGARCH)
 library(lubridate)
 
-rm(list=ls())
+rm(list = ls())
 source(file = "./data_import.R")
 
 
-### ----- fit of normal GARCH ---------
-spec_garch = ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)), mean.model = list(include.mean=FALSE), distribution.model = "norm")
+# ----- fit of normal GARCH ---------
+spec_garch = ugarchspec(
+  variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
+  mean.model = list(include.mean = FALSE),
+  distribution.model = "norm"
+)
 model = ugarchfit(spec_garch, spx.ret)
 model
 
-plot(model@fit[["sigma"]], type="l", ylab = "Sigma")
+plot(model@fit[["sigma"]], type = "l", ylab = "Sigma")
 
 
-### ----- GARCH-MIDAS with one explanatory variable--------
+# ----- GARCH-MIDAS with one explanatory variable --------
+## ----- Daily explanatory variables (VIX, Rvol22, vrp) -----
+
 ## s&p explained by vix
 df = merge(df_spx, df_vix, by = "date")
-GM_sp_vix = mfGARCH::fit_mfgarch(data = df, y = "spx", x = "vix", K = 3, low.freq = "date", weighting = "beta.restricted")
+GM_sp_vix = mfGARCH::fit_mfgarch(
+  data = df,
+  y = "spx",
+  x = "vix",
+  K = 3,
+  low.freq = "date",
+  weighting = "beta.restricted"
+)
 # plot_weighting_scheme(GM_sp_vix)
 
 ## s&p explained by Rvol22
 df = merge(df_spx, df_Rvol22, by = "date")
-GM_sp_Rvol22 = mfGARCH::fit_mfgarch(data = df, y = "spx", x = "Rvol22", K = 264, low.freq = "date", weighting = "beta.restricted")
+GM_sp_Rvol22 = mfGARCH::fit_mfgarch(
+  data = df,
+  y = "spx",
+  x = "Rvol22",
+  K = 264,
+  low.freq = "date",
+  weighting = "beta.restricted"
+)
 
 ## s&p explained by vrp
 df = merge(df_spx, df_vrp, by = "date")
-GM_sp_vrp = mfGARCH::fit_mfgarch(data = df, y = "spx", x = "vrp", K = 3, low.freq = "date", weighting = "beta.restricted")
+GM_sp_vrp = mfGARCH::fit_mfgarch(
+  data = df,
+  y = "spx",
+  x = "vrp",
+  K = 3,
+  low.freq = "date",
+  weighting = "beta.restricted"
+)
 
 
 # Focus on period 1990 - 2018 (as in the paper)
 df_2018 = df_spx %>% merge(df_vix, by = "date") %>% filter(year(date) < 2019)
 
-results_2018 = mfGARCH::fit_mfgarch(data = df_2018, y = "spx", x = "vix", K = 3, low.freq = "date", weighting = "beta.restricted")
-round(results_2018$par,4)
+results_2018 = mfGARCH::fit_mfgarch(
+  data = df_2018,
+  y = "spx",
+  x = "vix",
+  K = 3,
+  low.freq = "date",
+  weighting = "beta.restricted"
+)
+round(results_2018$par, 4)
 
 sum(is.na(df_2018$spx))
 
 # Rvol(22)
 df = df_spx %>% merge(df_Rvol22, by = "date") %>% filter(year(date) < 2019) %>% filter(1989 < year(date))
-GM_sp_Rvol22_restriced_period = mfGARCH::fit_mfgarch(data = df, y = "spx", x = "Rvol22", K = 264, low.freq = "date", weighting = "beta.restricted")
-round(GM_sp_Rvol22_restriced_period$par,4)
+GM_sp_Rvol22_restriced_period = mfGARCH::fit_mfgarch(
+  data = df,
+  y = "spx",
+  x = "Rvol22",
+  K = 264,
+  low.freq = "date",
+  weighting = "beta.restricted"
+)
+round(GM_sp_Rvol22_restriced_period$par, 4)
 
-### Non daily explanatory variable
+## ------ Non daily explanatory variable -------
+### dhoust
 df = df_spx %>% merge(df_dhoust, by = "date")
-GM_dhoust = mfGARCH::fit_mfgarch(data = df, y = "spx", x = "value", low.freq = "year_month", K =  36, weighting = "beta.unrestricted")
+GM_dhoust = mfGARCH::fit_mfgarch(
+  data = df,
+  y = "spx",
+  x = "value",
+  low.freq = "year_month",
+  K =  36,
+  weighting = "beta.unrestricted"
+)
 
+### nfci
 df = df_spx %>% merge(df_NFCI, by = "date")
-GM_nfci = mfGARCH::fit_mfgarch(data = df, y = "spx", x = "value", low.freq = "year_week", K =  52, weighting = "beta.restricted")
-GM_nfci$week_start <- 5 # should be the same as indicated in data_import.R
+GM_nfci = mfGARCH::fit_mfgarch(
+  data = df,
+  y = "spx",
+  x = "value",
+  low.freq = "year_week",
+  K =  52,
+  weighting = "beta.restricted"
+)
+GM_nfci$week_start <-
+  5 # should be the same as indicated in data_import.R
