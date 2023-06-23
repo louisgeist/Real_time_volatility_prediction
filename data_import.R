01b41608f893f4c7470327e129b580bcb918da65
Louis Geist <louisjjgeist@gmail.com>
2023-06-23 12:08:21 +0200

    date index translated for nfci (weekly updates)

--- data_import.R
+++ data_import.R
@@ -15,7 +15,6 @@ df_spx = import_spx()
 
 #---- External variables ------
 
-
 ## ----- Daily measures of financial risk -----
 
 ### RVol(22)
@@ -33,31 +32,11 @@ df_vrp = import_vrp()
 ### housing starts
 df_dhoust = import_houst()
 
-df_dhoust2 = import_houst()
-
-
-#################### from this point onwards, the new data is not automatically fetched.
-
 ### industrial production (ip)
-IP = read.csv("./data/IP_120623.csv")
-partial_df_ip = IP %>% rename(c("date" = "DATE", "value" = "IP"))
-df_ip = fill_missing_dates(partial_df_ip)
-
+df_ip = import_ip()
 
 ### nai
-nai = read.csv("./data/nai_120623.csv", sep = ";") %>% 
-  select(c("Date","CFNAI")) %>%
-  mutate(date = ym(Date),value = as.numeric(sub(",", ".", CFNAI, fixed = TRUE))) %>% 
-  select(-c("Date",CFNAI))
+df_nai = import_nai()
 
-df_nai = fill_missing_dates(nai, frequency = "month")
-
-ggplot(nai) + geom_line(aes(x = date, y = value))
-
-### nfci
-NFCI = read.csv("./data/nfci_120623.csv") %>%
-  mutate(date = mdy(Friday_of_Week)) %>%
-  select(c("date", "NFCI")) %>%
-  rename(c("value" = "NFCI"))
-
-df_NFCI = fill_missing_dates(NFCI, frequency = "week", week_start = 5)
+### nfci (weekly)
+df_nfci = import_nfci()
--- data_import_tools.R
+++ data_import_tools.R
@@ -4,20 +4,27 @@
 #In order to use properly "fit_mfgarch", we need to complete between two non consecutive dates the dataframe with the value of the first date ;
 #fill_missing_dates() solves this problem
 
-fill_missing_dates = function(partial_df, frequency = "month", week_start = FALSE) {
+fill_missing_dates = function(partial_df,
+                              frequency = "month",
+                              week_start = FALSE) {
   # partial_df must have two columns: "date" & "value"
   # if frequency = "week", then week_start needs an integer between 1 and 7 (Monday based)
   
-  list_day <- seq.Date(from = partial_df$date[1], to = Sys.Date(), by = "days")
+  list_day <-
+    seq.Date(from = partial_df$date[1],
+             to = Sys.Date(),
+             by = "days")
   list_value <- rep(NA_real_, length(list_day))
   
   i <- 1 # index going through partial_df
   
   for (day_index in seq_along(list_day)) {
     if (frequency == "week") {
-      day_floor <- lubridate::floor_date(list_day[day_index], unit = frequency, week_start)
+      day_floor <-
+        lubridate::floor_date(list_day[day_index], unit = frequency, week_start)
     } else {
-      day_floor <- lubridate::floor_date(list_day[day_index], unit = frequency)
+      day_floor <-
+        lubridate::floor_date(list_day[day_index], unit = frequency)
     }
     
     if (partial_df$date[i] == day_floor) {
@@ -30,7 +37,9 @@ fill_missing_dates = function(partial_df, frequency = "month", week_start = FALS
       if (partial_df$date[i] == day_floor) {
         list_value[day_index] <- partial_df$value[i]
       } else {
-        stop("Error in fill_missing_dates: partial_df$date is not ordered or a date is missing.")
+        stop(
+          "Error in fill_missing_dates: partial_df$date is not ordered or a date is missing."
+        )
       }
     }
   }
@@ -40,9 +49,11 @@ fill_missing_dates = function(partial_df, frequency = "month", week_start = FALS
     mutate(date = as.Date(date))
   
   if (frequency == "week") {
-    df <- df %>% mutate(year_week = lubridate::floor_date(date, unit = frequency, week_start))
+    df <-
+      df %>% mutate(year_week = lubridate::floor_date(date, unit = frequency, week_start))
   } else {
-    df <- df %>% mutate(year_month = lubridate::floor_date(date, unit = frequency))
+    df <-
+      df %>% mutate(year_month = lubridate::floor_date(date, unit = frequency))
   }
   
   return(df)
@@ -67,7 +78,7 @@ import_spx <- function() {
   return(df_spx)
 }
 
-import_Rvol22 <- function(){
+import_Rvol22 <- function() {
   spx.raw <-
     get.hist.quote(
       instrument = "^GSPC",
@@ -76,39 +87,51 @@ import_Rvol22 <- function(){
     )
   spx.ret = 100 * diff(log(spx.raw)) #stationary times series
   
-  Rvol22 = zoo::rollmean(spx.ret**2, 22, align = "right") #the current value and the past 21 values are taken for the mean
+  Rvol22 = zoo::rollmean(spx.ret ** 2, 22, align = "right") #the current value and the past 21 values are taken for the mean
   df_Rvol22 = Rvol22 %>% fortify.zoo() %>%  drop_na() %>% dplyr::rename(c("date" = "Index", "value" = "Close"))
   
   return(df_Rvol22)
 }
 
-import_vix <- function(){
-  vix.raw = get.hist.quote(instrument = "^VIX", start = as.Date("1990-01-01"), quote="Close")
+import_vix <- function() {
+  vix.raw = get.hist.quote(
+    instrument = "^VIX",
+    start = as.Date("1990-01-01"),
+    quote = "Close"
+  )
   sum(is.na(vix.raw$Close)) #numbers of NA 1990 to 09/06/2023 : 299
   
   df_vix = fortify.zoo(vix.raw)
   df_vix = df_vix  %>% drop_na()
-  df_vix = mutate(df_vix,vix_dailyscaled = Close/252**(1/2)) # vix which has been converted to a daily level
-  df_vix = df_vix %>% dplyr::rename(c("date" = "Index", "value" = "vix_dailyscaled")) %>% select(c("date","value"))
+  df_vix = mutate(df_vix, vix_dailyscaled = Close / 252 ** (1 / 2)) # vix which has been converted to a daily level
+  df_vix = df_vix %>% dplyr::rename(c("date" = "Index", "value" = "vix_dailyscaled")) %>% select(c("date", "value"))
   
   return(df_vix)
 }
 
-import_vrp <- function(){
-  spx.raw = get.hist.quote( instrument = "^GSPC", start = as.Date("1971-01-01"), quote = "Close")
+import_vrp <- function() {
+  spx.raw = get.hist.quote(
+    instrument = "^GSPC",
+    start = as.Date("1971-01-01"),
+    quote = "Close"
+  )
   spx.ret = 100 * diff(log(spx.raw)) #stationary times series
-  Rvol22 = zoo::rollmean(spx.ret**2, 22, align = "right") #the current value and the past 21 values are taken for the mean
+  Rvol22 = zoo::rollmean(spx.ret ** 2, 22, align = "right") #the current value and the past 21 values are taken for the mean
   
-  vix.raw = get.hist.quote(instrument = "^VIX", start = as.Date("1990-01-01"), quote="Close")
+  vix.raw = get.hist.quote(
+    instrument = "^VIX",
+    start = as.Date("1990-01-01"),
+    quote = "Close"
+  )
   
-  vrp = vix.raw/ 252**(1/2) - Rvol22
+  vrp = vix.raw / 252 ** (1 / 2) - Rvol22
   df_vrp = vrp %>% fortify.zoo() %>% drop_na() %>% dplyr::rename(c("date" = "Index", "value" = "Close"))
   
   return(df_vrp)
 }
 
 ## ----- non daily -----
-import_houst <- function(){
+import_houst <- function() {
   df_HOUST = alfred::get_alfred_series(
     series_id = "HOUST",
     series_name = "HOUST",
@@ -123,33 +146,45 @@ import_houst <- function(){
   return(df_dhoust)
 }
 
-import_ip <- function(){
+import_ip <- function() {
   df_ip_partial = alfred::get_alfred_series(
     series_id = "INDPRO",
     series_name = "IP",
     observation_start = "1959-01-01",
     realtime_start = ymd(today())
-  ) %>% as_tibble() %>% select(c("date","IP"))
+  ) %>% as_tibble() %>% select(c("date", "IP"))
   
   df_ip_partial$value = c(NA, 100 * diff(log(df_ip_partial$IP)))
-  df_ip = df_ip_partial %>% select(c("date","value")) %>% fill_missing_dates(frequency = "month") %>% as_tibble()
+  df_ip = df_ip_partial %>% select(c("date", "value")) %>% fill_missing_dates(frequency = "month") %>% as_tibble()
   
   return(df_ip)
 }
 
-import_nai <- function(){
+import_nai <- function() {
+  df_nai_partial = alfred::get_alfred_series(
+    series_id = "CFNAI",
+    series_name = "value",
+    observation_start = "1959-01-01",
+    realtime_start = ymd(today())
+  ) %>% as_tibble() %>% select(c("date", "value"))
   
+  
+  df_nai = df_nai_partial %>% fill_missing_dates(frequency = "month") %>% as_tibble()
+  
+  return(df_ip)
 }
 
-import_nfci <- function(){
+import_nfci <- function() {
   df_nfci_partial = alfred::get_alfred_series(
     series_id = "NFCI",
     series_name = "value",
     observation_start = "1959-01-01",
     realtime_start = ymd(today())
-  ) %>% as_tibble() %>% select(c("value","date"))
-  
-  df_nfci = fill_missing_dates(df_nfci_partial, frequency = "week", week_start = 5)
-  
-  return(df_nfci)
+  ) %>% as_tibble() %>% select(c("value", "date")) %>% mutate(date = date - 4) # we do this transformation, because the value of the friday corresponds to the week ending at that friday
+    
+    df_nfci = fill_missing_dates(df_nfci_partial,
+                                 frequency = "week",
+                                 week_start = 1)
+    
+    return(df_nfci)
 }
