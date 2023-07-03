5ea9b331584569d3b31d348c4c34dd7178d43493
Louis Geist <louisjjgeist@gmail.com>
2023-07-03 14:34:46 +0200

    eikon_test/ - initial commit of the folder

---
+++ eikon_test/eikon_test.py
@@ -0,0 +1,7 @@
+import eikon
+
+eikon.set_app_key('d3a3446b87854feeb530116eb2583ade9c0f0186')
+
+eikon.get_news_headlines(["MSFT.O"]) #, start_date = "2016-01-01", end_date = "2016-01-10"
+
+print("first hello")
