c394a3cec3bbd352bd4f241af93803fdf8112345
Louis Geist <louisjjgeist@gmail.com>
2023-07-03 15:58:34 +0200

    ip_nai comparison

--- Rshiny/ip_nai_comparison.R
+++ Rshiny/ip_nai_comparison.R
@@ -25,10 +25,30 @@ df_ip_partial$value = c(NA, 100 * diff(log(df_ip_partial$IP)))
 p = ggplot(df_ip_partial) + geom_line(aes(x = date , y = value))
 p
 
-
+p_raw_ip = ggplot(df_ip_partial) + geom_line(aes(x = date, y= IP))
+p_raw_ip
 
 # CFNAI
 
 df_nai = import_nai()
 p = ggplot(df_nai)+geom_line(aes(x = df_nai$date, y = df_nai$value))
 p
+
+# ----- comparison with Rebekka's data -----
+tab_ip = read.csv(file = "./data/IP_03072023.csv")
+tab_ip = tab_ip %>% select(c("date","IP"))
+
+tab_nai = read.csv("./data/nai_120623.csv", sep = ";") %>% 
+  select(c("Date","CFNAI")) %>%
+  mutate(date = ym(Date),value = as.numeric(sub(",", ".", CFNAI, fixed = TRUE))) %>% 
+  select(-c("Date",CFNAI))
+
+
+plot(tab_ip, type = "l")
+plot(tab_nai, type = "l")
+
+# mfGARCH data
+df_mfgarch = mfGARCH::df_mfgarch
+
+plot(x = df_mfgarch$date, y = df_mfgarch$nai, type = "l")
+plot(x = df_mfgarch$date, y = df_mfgarch$dindpro, type = "l")
