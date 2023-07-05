7359bce279f47a18e68bd4b9ecfe46a6dbe27170
Louis Geist <louisjjgeist@gmail.com>
2023-07-05 15:14:11 +0200

    eikon_test - try to link with Eikon API proxy

--- eikon_test/eikon_test.py
+++ eikon_test/eikon_test.py
@@ -2,16 +2,12 @@ import api_key
 import eikon
 import datetime
 
-print(api_key.my_api_key)
-# Définir votre clé d'accès Eikon
 eikon.set_app_key(api_key.my_api_key)
 
 
-# Définir la date de début et de fin souhaitée
 start_date = datetime.datetime(2023, 6, 1, 0, 0, 0)
 end_date = datetime.datetime(2023, 6, 2, 23, 59, 59)
 
-# Récupérer les valeurs de l'indice NASDAQ-100 avec un intervalle d'une minute
 data = eikon.get_timeseries('.NDX', start_date = start_date, end_date = end_date, interval='minute')
 
 #if err is None:
