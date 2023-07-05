8005f73fe227c226353717de5431cb00483fc680
Louis Geist <louisjjgeist@gmail.com>
2023-07-05 09:48:40 +0200

    eikon_test - clean

--- eikon_test/eikon_test.py
+++ eikon_test/eikon_test.py
@@ -1,7 +1,22 @@
+import api_key
 import eikon
+import datetime
 
-eikon.set_app_key('d3a3446b87854feeb530116eb2583ade9c0f0186')
+print(api_key.my_api_key)
+# Définir votre clé d'accès Eikon
+eikon.set_app_key(api_key.my_api_key)
 
-eikon.get_news_headlines(["MSFT.O"]) #, start_date = "2016-01-01", end_date = "2016-01-10"
 
-print("first hello")
+# Définir la date de début et de fin souhaitée
+start_date = datetime.datetime(2023, 6, 1, 0, 0, 0)
+end_date = datetime.datetime(2023, 6, 2, 23, 59, 59)
+
+# Récupérer les valeurs de l'indice NASDAQ-100 avec un intervalle d'une minute
+data = eikon.get_timeseries('.NDX', start_date = start_date, end_date = end_date, interval='minute')
+
+#if err is None:
+#    for timestamp in data.index:
+#        value = data.loc[timestamp]['CLOSE']
+#        print(f"Timestamp: {timestamp}, Value: {value}")
+#else:
+#    print(f"Erreur lors de la récupération des données : {err}")
