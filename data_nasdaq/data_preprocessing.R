3401cff25b54d57bd1ad6529edc0660d982431b3
Louis Geist <louisjjgeist@gmail.com>
2023-07-06 16:35:34 +0200

    data_nasdaq - compute of the daily RV - alpha

--- data_nasdaq/data_preprocessing.R
+++ data_nasdaq/data_preprocessing.R
@@ -1,50 +1,49 @@
-df = read.csv(file = "./data_nasdaq/NDX_daily_20230705.csv")
 
-df_spx %>% tail()
-
-
-import_ndx <- function() {
-  # Automatic import of NASDAQ-100 from Yahoo finance
-  ndx.raw <-
-    get.hist.quote(
-      instrument = "^NDX",
-      start = as.Date("1971-01-01"),
-      quote = "Close"
-    )
-  ndx.ret = 100 * diff(log(ndx.raw)) #stationary times series
-  
-  df_ndx = fortify.zoo(ndx.ret)
-  df_ndx = dplyr::rename(df_ndx, c("date" = "Index", "ndx" = "Close")) %>% as_tibble()
-  
-  return(df_ndx)
-}
-
-df_ndx = import_ndx()
+df_ndx = import_ndx() #import of daily ndx
 df_ndx
 
 
 p = ggplot(df_ndx) + geom_line(aes(x = date, y = ndx))
+p
 
-df_ndx %>% as_tibble()
 
-# ---- data import -----
+# ---- compute of the volatility -----
+
 library(readxl)
 
 five_min_ndx = read_excel("./data_nasdaq/ndx_20230705_c.xlsx") %>% dplyr::rename("date" = "Local Date")
 five_min_ndx = five_min_ndx  %>%  as_tibble()
 
 x = five_min_ndx
+x_bis = x %>% mutate(day_date = day(date))
 
-compute_realized_volatility <- function(x){
-  
+compute_realized_volatility <- function(x) {
   x$log_return = c(diff(log(x$Close)), NA)
-  x$square = x$log_return**2
+  x$square = x$log_return ** 2
   
-  df = x %>% group_by(date) %>% summarise(RV = mean(square))
+  df = x %>% 
+    group_by(lubridate::date(date)) %>% 
+    dplyr::summarise(RV = sum(square)) %>% 
+    dplyr::rename("date" = "lubridate::date(date)")
   
   return(df)
 }
 
-test = compute_realized_volatility(x)
+RV = compute_realized_volatility(x)
 
-x 
+# plots
+p_rv = ggplot(RV) + geom_point(aes(x = date, y = RV))
+p_rv
+
+
+x$log_return = c(diff(log(x$Close)), NA)
+x$square = x$log_return ** 2
+
+p = ggplot(x) + geom_line(aes(x = date, y = Close))
+p
+
+p = ggplot(x) + geom_line(aes(x = date, y = log_return))
+p
+
+p_square = ggplot(x) + geom_line(aes(x = date, y = square))
+p_square
