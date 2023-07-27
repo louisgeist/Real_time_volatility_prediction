00b18a67ec0772b425868f5ea7885c77781b0609
Louis Geist <louisjjgeist@gmail.com>
2023-07-27 16:51:08 +0200

    validation.R - filter & weights update

--- validation.R
+++ validation.R
@@ -14,16 +14,16 @@ library(rugarch)
 main_index = "spx"
 
 # ----- 0.Tool ------ 
-filter_data = function(last_date, df_main_index_raw, df_dhoust_raw, df_ip_raw, df_nai_raw, df_nfci_raw, df_vix_raw, df_vrp_raw, df_Rvol22_raw) {
-  # Find the index where the condition is true
-  index_main_index <- which(df_main_index_raw$date < last_date)
-  index_dhoust <- which(floor_date(df_dhoust_raw$date, unit = "month") < floor_date(last_date, unit = "month"))
-  index_ip <- which(floor_date(df_ip_raw$date, unit = "month") < floor_date(last_date, unit = "month"))
-  index_nai <- which(floor_date(df_nai_raw$date, unit = "month") < floor_date(last_date, unit = "month"))
-  index_nfci <- which(floor_date(df_nfci_raw$date, unit = "week", 5) < floor_date(last_date, unit = "week", 5))
-  index_vix <- which(df_vix_raw$date < last_date)
-  index_vrp <- which(df_vrp_raw$date < last_date)
-  index_Rvol22 <- which(df_Rvol22_raw$date < last_date)
+filter_data = function(first_date, last_date, df_main_index_raw, df_dhoust_raw, df_ip_raw, df_nai_raw, df_nfci_raw, df_vix_raw, df_vrp_raw, df_Rvol22_raw) {
+  # Find the index where the conditions are true
+  index_main_index <- which(df_main_index_raw$date >= first_date & df_main_index_raw$date < last_date)
+  index_dhoust <- which(floor_date(df_dhoust_raw$date, unit = "month") >= floor_date(first_date, unit = "month") & floor_date(df_dhoust_raw$date, unit = "month") < floor_date(last_date, unit = "month"))
+  index_ip <- which(floor_date(df_ip_raw$date, unit = "month") >= floor_date(first_date, unit = "month") & floor_date(df_ip_raw$date, unit = "month") < floor_date(last_date, unit = "month"))
+  index_nai <- which(floor_date(df_nai_raw$date, unit = "month") >= floor_date(first_date, unit = "month") & floor_date(df_nai_raw$date, unit = "month") < floor_date(last_date, unit = "month"))
+  index_nfci <- which(floor_date(df_nfci_raw$date, unit = "week", 5) >= floor_date(first_date, unit = "week", 5) & floor_date(df_nfci_raw$date, unit = "week", 5) < floor_date(last_date, unit = "week", 5))
+  index_vix <- which(df_vix_raw$date >= first_date & df_vix_raw$date < last_date)
+  index_vrp <- which(df_vrp_raw$date >= first_date & df_vrp_raw$date < last_date)
+  index_Rvol22 <- which(df_Rvol22_raw$date >= first_date & df_Rvol22_raw$date < last_date)
   
   # Subset the DataFrames using the index
   df_main_index <<- df_main_index_raw[index_main_index, ]
@@ -31,10 +31,9 @@ filter_data = function(last_date, df_main_index_raw, df_dhoust_raw, df_ip_raw, d
   df_ip <<- df_ip_raw[index_ip, ]
   df_nai <<- df_nai_raw[index_nai, ]
   df_nfci <<- df_nfci_raw[index_nfci, ]
-  df_vix  <<- df_vix_raw[index_vix, ]
-  df_vrp  <<- df_vrp_raw[index_vrp, ]
-  df_Rvol22  <<- df_Rvol22_raw[index_Rvol22, ]
-  
+  df_vix <<- df_vix_raw[index_vix, ]
+  df_vrp <<- df_vrp_raw[index_vrp, ]
+  df_Rvol22 <<- df_Rvol22_raw[index_Rvol22, ]
 }
 
 qlike <- function(h,sigma_square){
@@ -59,9 +58,10 @@ df_Rvol22_raw = df_Rvol22
 
 
 # ------ 2. Training ------
+date_begin_training = ymd("1971-01-05") # first day of availibity of S&P, (NDX is available later)
 date_end_training = ymd("2015-01-01")
 
-filter_data(date_end_training, df_main_index_raw, df_dhoust_raw, df_ip_raw, df_nai_raw, df_nfci_raw, df_vix_raw, df_vrp_raw, df_Rvol22_raw)
+filter_data(first_date = date_begin_training, last_date = date_end_training, df_main_index_raw, df_dhoust_raw, df_ip_raw, df_nai_raw, df_nfci_raw, df_vix_raw, df_vrp_raw, df_Rvol22_raw)
 
 
 source("./estimation.R")
@@ -69,9 +69,8 @@ source("./estimation.R")
 # ------ 3. Forecasts & evaluation------
 source("./forecast.R")
 
-
-n_forecasts = 1 #number of days for the test set
-date_to_forecast = seq_quotation_date(date_end_training, n_forecasts) # function implemented in forecast.R
+n_forecasts = 10 #number of days for the test set
+date_to_forecast = seq_quotation_date(date_end_training, n_forecasts-1) # function implemented in forecast.R
 
 
 ## --- Test set ---
@@ -83,12 +82,12 @@ df_date_to_forecast = data.frame(date = date_to_forecast) %>%
   merge(realized_library, by = "date")
 
 ## --- Forecasts on the test set ---
-
+h_lists = c(1,5)
 h = 1 # one day forecast
 
 ### ---- GARCH-MIDAS models part ------
-GM_models_list = c("GM_dhoust","GM_ip","GM_nai","GM_nfci","GM_Rvol22", "GM_vix","GM_vrp","GM_vix_dhoust", "GM_vix_ip", "GM_vix_nai", "GM_vix_nfci")
-
+#GM_models_list = c("GM_dhoust","GM_ip","GM_nai","GM_nfci","GM_Rvol22", "GM_vix","GM_vrp","GM_vix_dhoust", "GM_vix_ip", "GM_vix_nai", "GM_vix_nfci") #GM_Rvol_22 -> temporarly removed because K=264 makes forecasting really two slow
+GM_models_list = c("GM_vix")
 
 error_container <- list()
 for (model in GM_models_list) {
@@ -98,10 +97,28 @@ for (model in GM_models_list) {
 
 Rprof(interval = 0.05)
 
+first_date_for_forecasts = date_end_training - years(1) #needs to be old enough so that enough lags are included
+
+
+filter_data(first_date = first_date_for_forecasts, last_date = final_date, df_main_index_raw, df_dhoust_raw, df_ip_raw, df_nai_raw, df_nfci_raw, df_vix_raw, df_vrp_raw, df_Rvol22_raw)
+
+# WATCH OUT : if needed, old data needs to be reimported
+df_main_index_raw <- df_main_index
+df_dhoust_raw <- df_dhoust
+df_ip_raw <- df_ip
+df_nai_raw <- df_nai
+df_nfci_raw <- df_nfci
+df_vix_raw <- df_vix
+df_vrp_raw <- df_vrp
+df_Rvol22_raw <- df_Rvol22
+# WATCH OUT
+
 for(i in seq_along(date_to_forecast)){
   print(date_to_forecast[[i]])
   
-  filter_data(ymd(date_to_forecast[[i]]), df_main_index_raw, df_dhoust_raw, df_ip_raw, df_nai_raw, df_nfci_raw, df_vix_raw, df_vrp_raw, df_Rvol22_raw)#data available until the previous day of forecast (because we forecast at horizon 1 at the moment)
+  filter_data(first_date = first_date_for_forecasts, last_date = ymd(date_to_forecast[[i]]),
+              df_main_index_raw, df_dhoust_raw, df_ip_raw, df_nai_raw, df_nfci_raw, df_vix_raw, df_vrp_raw, df_Rvol22_raw
+              ) #data available until the previous day of forecast (because we forecast at horizon 1 at the moment)
   
   real_volatility = df_date_to_forecast$rv5[[i]] * 10**4 #because we have multiplied the returns by 10**2 #as.Date(current_date)
   
@@ -127,7 +144,6 @@ for(i in seq_along(date_to_forecast)){
   
   #df_error = data.frame(date = date_to_forecast, GM_dhoust = error_dhoust)
 }
-  
 
 Rprof(NULL)
 
