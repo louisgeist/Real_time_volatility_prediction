aafc5e12380c3b193f0103e216066b801c09bb81
Louis Geist <louisjjgeist@gmail.com>
2023-08-04 10:26:49 +0200

    eikon_test - new test

--- eikon_test/eikon_test.py
+++ eikon_test/eikon_test.py
@@ -8,11 +8,5 @@ eikon.set_app_key(api_key.my_api_key)
 start_date = datetime.datetime(2023, 6, 1, 0, 0, 0)
 end_date = datetime.datetime(2023, 6, 2, 23, 59, 59)
 
-data = eikon.get_timeseries('.NDX', start_date = start_date, end_date = end_date, interval='minute')
-
-#if err is None:
-#    for timestamp in data.index:
-#        value = data.loc[timestamp]['CLOSE']
-#        print(f"Timestamp: {timestamp}, Value: {value}")
-#else:
-#    print(f"Erreur lors de la récupération des données : {err}")
+#data = eikon.get_timeseries('.NDX', start_date = start_date, end_date = end_date, interval='minute')
+data = eikon.get_timeseries('AAPL.O', interval='minute')
