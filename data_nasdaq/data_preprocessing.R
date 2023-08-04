e88dda4913a5bf54a51a44417e82e9557430a284
Louis Geist <louisjjgeist@gmail.com>
2023-08-04 10:27:53 +0200

    data_nasdaq - preprocess of eikon data

--- data_nasdaq/data_preprocessing.R
+++ data_nasdaq/data_preprocessing.R
@@ -1,15 +1,17 @@
 
+
 df_ndx = import_ndx() #import of daily ndx
 df_ndx
-
-
-p = ggplot(df_ndx) + geom_line(aes(x = date, y = ndx))
-p
 
 
 # ---- compute of the volatility -----
 
 library(readxl)
+library(highfrequency)
+library(ggplot2)
+
+
+#write.csv(realized_library, file = "./realized_library.csv")
 
 five_min_ndx = read_excel("./data_nasdaq/ndx_20230705_c.xlsx") %>% dplyr::rename("date" = "Local Date")
 five_min_ndx = five_min_ndx  %>%  as_tibble()
@@ -18,32 +20,32 @@ x = five_min_ndx
 x_bis = x %>% mutate(day_date = day(date))
 
 compute_realized_volatility <- function(x) {
-  x$log_return = c(diff(log(x$Close)), NA)
+  x$log_return = 100 * c(diff(log(x$Close)), NA)
   x$square = x$log_return ** 2
   
   df = x %>% 
     group_by(lubridate::date(date)) %>% 
-    dplyr::summarise(RV = sum(square)) %>% 
-    dplyr::rename("date" = "lubridate::date(date)")
+    dplyr::summarise(RV = mean(square)) %>% 
+    dplyr::rename("date" = "lubridate::date(date)") %>% 
+    drop_na()
+
+  df$RV = df$RV
   
   return(df)
 }
 
-RV = compute_realized_volatility(x)
+RV_mafonction = compute_realized_volatility(x)
+
+
+RV_highfreq_computation = highfrequency::RV(five_min_ndx$Close, )
+
+
 
 # plots
-p_rv = ggplot(RV) + geom_point(aes(x = date, y = RV))
+p_rv = ggplot(RV) + geom_point(aes(x = date, y = sqrt(RV)))
 p_rv
 
+# plot with highfrequency data
+p_rv_hf = ggplot(realized_library) + geom_line(aes(x = date,y = sqrt(rv5)))
+p_rv_hf
 
-x$log_return = c(diff(log(x$Close)), NA)
-x$square = x$log_return ** 2
-
-p = ggplot(x) + geom_line(aes(x = date, y = Close))
-p
-
-p = ggplot(x) + geom_line(aes(x = date, y = log_return))
-p
-
-p_square = ggplot(x) + geom_line(aes(x = date, y = square))
-p_square
