25edc505b9c1f27622019d64d6cf2632f9f8728c
Louis Geist <louisjjgeist@gmail.com>
2023-08-23 16:49:50 +0200

    qlike.error_analsysis.R - Rshiny usage update

--- qlike.error_analysis.R
+++ qlike.error_analysis.R
@@ -1,15 +1,15 @@
 # uses the results of "validation.R"
 
 error_array_analysis <- function(error_array, GM_models_list, h_list){
-  print(paste0("Number of predictions per horizon and per model : ", dim(error_array)[[2]]))
-  
+  #print(paste0("Number of predictions per horizon and per model : ", dim(error_array)[[2]]))
+
   # compute of the columns
   error_mean = aaply(error_array, c(1,3), .fun = mean)
-  
+
   row.names(error_mean) <- GM_models_list
   colnames(error_mean) <- h_list
   
-  print(error_mean)
+  #print(error_mean)
   
   # 
   min_indices <- apply(error_mean, 2, which.min)
@@ -24,6 +24,8 @@ error_array_analysis <- function(error_array, GM_models_list, h_list){
     error_mean_min[,i] <- error_mean[,i]== min_list[[i]]
   }
   
-  print(error_mean_min)
+  matrix_list = list(error_mean = error_mean,
+                error_mean_min = error_mean_min)
   
+  return(matrix_list)
 }
