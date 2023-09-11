704c11ca6ce69c78132a7cdea6a92cfe657ed613
Louis Geist <louisjjgeist@gmail.com>
2023-09-11 12:30:53 +0200

    CI - bug fix always same df_RV

--- confidence_interval.R
+++ confidence_interval.R
@@ -8,18 +8,17 @@ library(readxl)
 library(lubridate)
 
 # ----- Parameters -----
-main_index = "ndx"
+main_index = "spx"
 models <- c("GM_dhoust", "GM_ip", "GM_nai", "GM_nfci", "GM_Rvol22", "GM_vix", "GM_vrp", "GM_vix_dhoust", "GM_vix_ip", "GM_vix_nai", "GM_vix_nfci", "GARCH11")
 h_list = 1:66
-list_origin_date = seq_quotation_date(ymd("2023-05-01"), 69 - 10) # 68 because the 68th day is the last day of available data when I am writing the script
-                                                                  # -10 to have enough date even for 10 days ahead
+
 
 h_list_ic <- 1:10
 
 if(main_index == "spx"){
-  path_data_eikon = "../data_eikon/spx_29_08_23.xlsx"
+  path_data_eikon = "../data_eikon/spx_10_09_23.xlsx"
 }else{
-  path_data_eikon = "../data_eikon/ndx_31_08_23.xlsx"
+  path_data_eikon = "../data_eikon/ndx_10_09_23.xlsx"
 }
 
 
@@ -27,6 +26,9 @@ if(main_index == "spx"){
 # eikon data
 five_min_data = read_excel(path_data_eikon) %>% dplyr::rename("date" = "Local Date")
 df_RV = compute_realized_volatility(five_min_data)
+
+
+list_origin_date = seq_quotation_date(df_RV$date[[1]]-days(1), 69 - 10)
 
 #----- 2. ratio array build ----
 ratio_array <- array(0, dim = c(length(models),length(h_list_ic),length(list_origin_date)))
@@ -55,7 +57,7 @@ for(i_date in seq_along(list_origin_date)){
 sorted_ratio_array = aaply(ratio_array, c(1,2), .fun = sort)
 
 #---- 3. Quantile computation ----
-level = 0.9
+level = 0.5
 
 n_lower_quantile = (1-level) / 2 *length(list_origin_date) %>% floor() #watch out that the round doesn't alter to much the level
 n_upper_quantile = (1- (1-level) /2)* length(list_origin_date) %>% floor()
@@ -67,4 +69,20 @@ get_quantiles <- function(vector){
 
 quantile_array <- aaply(sorted_ratio_array, c(1,2), .fun = get_quantiles)
 
-saveRDS(quantile_array, file = paste0("../data_daily_forecast/",main_index,"/quantile_array.rds"))
+saveRDS(quantile_array, file = paste0("../data_daily_forecast/",main_index,"/quantile_array_",level,".rds"))
+
+
+# ---- 4. Analysis of the results ----
+plot(sorted_ratio_array[1,1,])
+plot(sorted_ratio_array[1,10,])
+
+for(i in 1:10){
+  df = data.frame(index = 1:60, ratio = sorted_ratio_array[6,i,])
+  p = plot_ly(df) %>% 
+    add_markers(x = ~index, y = ~ratio) %>% 
+    layout(title = paste0("sorted ratio array, for vix and at horizon ", i))
+  print(p)
+}
+
+df = data.frame(index = 1:60, ratio = sorted_ratio_array[6,1,])
+plot_ly(df) %>% add_markers(x = ~index, y = ~ratio)
