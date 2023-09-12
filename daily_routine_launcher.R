fb556432896fa86565d869877e24ecb6756d2b85
Louis Geist <louisjjgeist@gmail.com>
2023-09-12 10:19:03 +0200

    daily_rountine_launcher.R - week-end launch bug fixed

--- daily_routine_launcher.R
+++ daily_routine_launcher.R
@@ -1,9 +1,14 @@
 library(lubridate)
+source("./forecast.R")
 
 origin_date = today() - days(1)
+t = seq_quotation_date(origin_date,0)
 
-main_index = "spx"
-source("./daily_routine_v2.R")
-
-main_index = "ndx"
-source("./daily_routine_v2.R")
+if(t == today()-days(1)){
+  main_index = "spx"
+  source("./daily_routine_v2.R")
+  
+  main_index = "ndx"
+  source("./daily_routine_v2.R")
+}
+  
